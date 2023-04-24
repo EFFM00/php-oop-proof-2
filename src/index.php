024c3990226e8ceba02803f693a731a851ff19f5
EFFM00 <fischietto.elena@gmail.com>
2023-04-24 19:49:59 -0300

    clase producto

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Elena\PhpOopProof2\models\Producto;
+
+
+$x = new Producto("x", 223.45, 3.1);
+$y = new Producto("y", 5000.39, 1.13);
+$z = new Producto("x", 102.56, 4.8);
---
+++ src/models/Producto.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Elena\PhpOopProof2\models;
+
+class Producto {
+
+
+    private string $nombre;
+    private float $precio;
+    private float $peso;
+
+
+    public function __construct(
+        string $nombre,
+        float $precio,
+        float $peso
+    ) {
+
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->peso = $nombre;
+
+    }
+
+
+    
+    /**
+     * Get the value of peso
+     */ 
+    public function getPeso()
+    {
+        return $this->peso;
+    }
+
+    /**
+     * Set the value of peso
+     *
+     * @return  self
+     */ 
+    public function setPeso($peso)
+    {
+        $this->peso = $peso;
+
+        return $this;
+    }
+
+
+
+    /**
+     * Get the value of precio
+     */ 
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    /**
+     * Set the value of precio
+     *
+     * @return  self
+     */ 
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+
+    
+    /**
+     * Get the value of nombre
+     */ 
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @return  self
+     */ 
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+}
