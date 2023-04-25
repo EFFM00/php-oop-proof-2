3a5d6cad7c8b8d131e8283cff3c9930a93d30ab2
EFFM00 <fischietto.elena@gmail.com>
2023-04-24 21:51:55 -0300

    falta testing, pero terminado

--- index.php
+++ index.php
@@ -4,13 +4,3 @@ require 'vendor/autoload.php';
 
 use Elena\PhpOopProof2\models\Producto;
 use Elena\PhpOopProof2\models\Carrito;
-
-
-$x = new Producto("x", 223.45, 3.1);
-$y = new Producto("y", 5000.39, 1.13);
-$z = new Producto("x", 102.56, 4.8);
-
-
-print_r($x->toString());
-print_r($y->toString());
-print_r($z->toString());
--- test/CarritoTests.php
+++ test/CarritoTests.php
@@ -2,6 +2,8 @@
 
 use PHPUnit\Framework\TestCase;
 
+use Elena\PhpOopProof2\models\Producto;
+use Elena\PhpOopProof2\models\Carrito;
 
 class CarritoTests extends TestCase {
 
@@ -9,11 +11,26 @@ class CarritoTests extends TestCase {
 
     public function setUp(): void {
         $this->$carrito = new Carrito();
+
+        var_dump($this->carrito);
     }
 
 
     public function testAgregarProductos() {
         
+        $x = new Producto("x", 223.45, 3.1);
+        // $y = new Producto("y", 5000.39, 1.13);
+        // $z = new Producto("x", 102.56, 4.8);
+        
+        // $carrito[] = $x;
+        // $carrito[] = $y;
+        $carrito[] = $z;
+
+        // $this->carrito->agregarProducto($x);
+        // $this->carrito->agregarProducto($y);
+        $this->carrito->agregarProducto($z);
+
+        $this->assertTrue($carrito->contarCantidadProductos() === 1);
     }
 
 }
