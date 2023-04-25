58dc839189030dfadd5137ae50b1c09d57a976d7
EFFM00 <fischietto.elena@gmail.com>
2023-04-24 21:39:04 -0300

    metodos carrito y test iniciado

--- src/models/Carrito.php
+++ src/models/Carrito.php
@@ -19,4 +19,35 @@ class Carrito {
 
         $this->estado->setCarrito($this);
     }
+
+
+    public function agregarProducto(Producto $producto):void {
+        $this->estado->agregarProducto($producto);
+    }
+    
+    
+    public function sacarProducto(Producto $producto):void {
+        $this->estado->sacarProducto($producto);
+    }
+    
+    
+    public function contarCantidadProductos():int {
+        $this->estado->contarCantidadProductos();
+    }
+    
+    
+    public function subtotalPorProducto(Producto $producto):float {
+        $this->estado->subtotalPorProducto($producto);
+    }
+    
+    
+    public function montoTotal():float {
+        $this->montoTotal->montoTotal();
+    }
+    
+    
+    public function verificarPesoPermitido():bool {
+        $this->montoTotal->verificarPesoPermitido();
+    }
+
 }
---
+++ test/CarritoTests.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+
+class CarritoTests extends TestCase {
+
+    private $carrito;
+
+    public function setUp(): void {
+        $this->$carrito = new Carrito();
+    }
+
+
+    public function testAgregarProductos() {
+        
+    }
+
+}
